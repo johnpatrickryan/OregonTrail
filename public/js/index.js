33f75b2dde331095cf79e1c7313fa31be4996147
John Ryan <john.ryan4@marist.edu>
2017-10-16 22:30:00 -0400

    adding routes

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,9 @@
 document.onkeypress = function(e) {
     if(e.keyCode == 32){
-        window.location.href = "http://localhost:1337/mainmenu"
+        window.location.href = "http://148.100.5.112:1337/mainmenu"
     }
 } 
 
 document.getElementById("spacebarText").onclick = function() {
-    window.location.href = "http://localhost:1337/mainmenu";
+    window.location.href = "http://148.100.5.112:1337/mainmenu";
 }
